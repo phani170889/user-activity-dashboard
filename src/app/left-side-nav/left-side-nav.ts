01bfa9515d59f5e9bfcfefae52afada53aff9989
phani170889 <phanindram17@gmail.com>
2025-10-16 19:42:56 -0400

    code refactor and cleanup

--- src/app/left-side-nav/left-side-nav.ts
+++ src/app/left-side-nav/left-side-nav.ts
@@ -1,63 +1,36 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatSidenavContainer, MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatToolbarModule, MatToolbarRow } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatMenuModule, MatMenuPanel } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
-import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
-import type { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+import { AgGridTable } from '../ag-grid-table/ag-grid-table';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-left-side-nav',
   imports: [
     MatSidenavModule,
     MatMenuModule,
-    MatSidenavContainer,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
     MatTabsModule,
     MatToolbarModule,
-    AgGridAngular,
+    MatToolbarRow,
+    MatListModule,
+    AgGridTable,
   ],
   templateUrl: './left-side-nav.html',
   styleUrl: './left-side-nav.scss',
 })
 export class LeftSideNav implements OnInit {
-  @Input() profileData: any;
-  menu: any;
-  rowData: any;
-  colDefs: ColDef[] = [{ field: 'firstName' }, { field: 'lastName' }, { field: 'zipCode' }];
-  tableData: any[] = [];
-
+  openMenu: boolean = false;
+  drawer: any;
+  toggleDrawer() {
+    this.openMenu = !this.openMenu;
+  }
   ngOnInit() {}
-
-  ngOnChanges() {
-    console.log('profileData', this.profileData);
-    if (this.profileData) {
-      this.rowData = this.getTableRows(this.profileData);
-    }
-  }
-
-  getTableRows(data: any) {
-    data.forEach((ele: any) => {
-      this.tableData.push({
-        firstName: ele.first_name,
-        lastName: ele.last_name,
-        zipCode: ele.zip_code,
-      });
-    });
-    return this.tableData;
-  }
 }
-
-// Row Data: The data to be displayed.
-// rowData = [
-//     { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-//     { make: "Ford", model: "F-Series", price: 33850, electric: false },
-//     { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-// ];
-
-// Column Definitions: Defines the columns to be displayed.
